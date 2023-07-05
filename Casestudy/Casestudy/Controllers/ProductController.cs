ae9b2efc983bd43543928a184a8ef806df12d300
Gui Miranda <gccardosomiranda@outlook.com>
2023-07-05 10:29:42 -0400

    Adds server-side functionality to get Products based on Brands

---
+++ Casestudy/Casestudy/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Casestudy.DAL;
+using Casestudy.DAL.DAO;
+using Casestudy.DAL.DomainClasses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casestudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly AppDbContext? _ctx;
+        public ProductController(AppDbContext context) // injected here
+        {
+            _ctx = context;
+        }
+
+        [HttpGet]
+        [Route("{brandid}")]
+        public async Task<ActionResult<List<Product>>> Index(int brandid)
+        {
+            ProductDAO dao = new(_ctx!);
+            List<Product> itemsForBrand = await dao.GetAllByBrand(brandid);
+            return itemsForBrand;
+        }
+    }
+}
---
+++ Casestudy/Casestudy/DAL/DAO/ProductDAO.cs
@@ -0,0 +1,18 @@
+﻿using Casestudy.DAL.DomainClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Casestudy.DAL.DAO
+{
+    public class ProductDAO
+    {
+        private readonly AppDbContext _db;
+        public ProductDAO(AppDbContext ctx)
+        {
+            _db = ctx;
+        }
+        public async Task<List<Product>> GetAllByBrand(int id)
+        {
+            return await _db.Products!.Where(item => item.Brand!.Id == id).ToListAsync();
+        }
+    }
+}
