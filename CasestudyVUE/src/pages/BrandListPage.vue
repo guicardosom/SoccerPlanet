da0562ffa3e135b4a8a1b9d9016d547e7154e0be
Gui Miranda <gccardosomiranda@outlook.com>
2023-07-11 11:09:17 -0400

    Displays product's specific info when clicked on and the ability to view the cart

--- CasestudyVUE/src/pages/BrandListPage.vue
+++ CasestudyVUE/src/pages/BrandListPage.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="text-center">
-    <q-avatar class="q-mb-md" style="width: 200px; height: 100px" square>
-      <q-img :src="`/img/logo.png`" />
-    </q-avatar>
-
     <div class="text-h2 q-mt-lg">Brands</div>
 
     <div class="status q-mt-md text-subtitle2 text-negative" text-color="red">
@@ -33,9 +29,14 @@
     <q-scroll-area style="height: 55vh">
       <q-card class="q-ma-md">
         <q-list separator>
-          <q-item clickable v-for="product in state.products" :key="product.id">
+          <q-item
+            clickable
+            v-for="product in state.products"
+            :key="product.id"
+            @click="selectProduct(product.id)"
+          >
             <q-item-section avatar>
-              <q-avatar style="height: 125px; width: 90px" square>
+              <q-avatar style="height: 75px; width: 75px" square>
                 <img :src="`/img/${product.graphicName}`" />
               </q-avatar>
             </q-item-section>
@@ -47,14 +48,88 @@
       </q-card>
     </q-scroll-area>
   </div>
+
+  <q-dialog
+    v-model="state.itemSelected"
+    transition-show="rotate"
+    transition-hide="rotate"
+  >
+    <q-card>
+      <q-card-actions align="right">
+        <q-btn flat label="X" color="primary" v-close-popup class="text-h5" />
+      </q-card-actions>
+
+      <q-card-section class="text-subtitle2 text-center">
+        <q-avatar style="height: 150px; width: 150px" square>
+          <img :src="`/img/${state.selectedProduct.graphicName}`" />
+        </q-avatar>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="text-subtitle2 text-center">
+          {{ state.selectedProduct.productName }} -
+          {{ formatCurrency(state.selectedProduct.msrp) }}
+        </div>
+      </q-card-section>
+
+      <q-card-section class="text-subtitle2 text-center">
+        <q-chip icon="bookmark" color="primary" text-color="white" text-center
+          >Details
+          <q-tooltip
+            transition-show="flip-right"
+            transition-hide="flip-left"
+            text-color="white"
+          >
+            {{ state.selectedProduct.description }}
+          </q-tooltip>
+        </q-chip>
+      </q-card-section>
+
+      <q-card-section>
+        <div class="row">
+          <q-input
+            v-model.number="state.qty"
+            type="number"
+            filled
+            placeholder="qty"
+            hint="Qty"
+            dense
+            style="max-width: 12vw"
+          />&nbsp;
+          <q-btn
+            color="primary"
+            label="Add To Cart"
+            :disable="state.qty < 0"
+            @click="addToCart()"
+            style="max-width: 25vw; margin-left: 3vw"
+          />
+          <q-btn
+            color="primary"
+            label="View Cart"
+            :disable="state.qty < 0"
+            @click="viewCart()"
+            style="max-width: 25vw; margin-left: 3vw"
+          />
+        </div>
+      </q-card-section>
+
+      <q-card-section class="text-center text-positive">
+        {{ state.dialogStatus }}
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
 import { reactive, onMounted } from "vue";
+import { useRouter } from "vue-router";
 import { fetcher } from "../utils/apiutil";
+import { formatCurrency } from "../utils/formatutils";
 
 export default {
   setup() {
+    const router = useRouter();
+
     onMounted(() => {
       loadBrands();
     });
@@ -65,6 +140,11 @@ export default {
       products: [],
       selectedBrand: {},
       selectedBrandId: "",
+      selectedProduct: {},
+      dialogStatus: "",
+      itemSelected: false,
+      qty: 0,
+      cart: [],
     });
 
     const loadBrands = async () => {
@@ -91,10 +171,64 @@ export default {
       }
     };
 
+    const selectProduct = async (productId) => {
+      try {
+        state.selectedProduct = state.products.find(
+          (item) => item.id === productId
+        );
+        state.itemSelected = true;
+        state.dialogStatus = "";
+        if (sessionStorage.getItem("cart")) {
+          state.cart = JSON.parse(sessionStorage.getItem("cart"));
+        }
+      } catch (err) {
+        console.log(err);
+        state.status = `Error has occured: ${err.message}`;
+      }
+    };
+
+    const addToCart = () => {
+      let index = -1;
+      if (state.cart.length > 0) {
+        index = state.cart.findIndex(
+          // is item already on the cart
+          (item) => item.id === state.selectedProduct.id
+        );
+      }
+      if (state.qty > 0) {
+        index === -1 // add
+          ? state.cart.push({
+              id: state.selectedProduct.id,
+              qty: state.qty,
+              item: state.selectedProduct,
+            })
+          : (state.cart[index] = {
+              // replace
+              id: state.selectedProduct.id,
+              qty: state.qty,
+              item: state.selectedProduct,
+            });
+        state.dialogStatus = `${state.qty} product(s) added`;
+      } else {
+        index === -1 ? null : state.cart.splice(index, 1); // remove
+        state.dialogStatus = `product(s) removed`;
+      }
+      sessionStorage.setItem("cart", JSON.stringify(state.cart));
+      state.qty = 0;
+    };
+
+    const viewCart = () => {
+      router.push("cart");
+    };
+
     return {
       state,
       loadBrands,
       getProducts,
+      selectProduct,
+      addToCart,
+      formatCurrency,
+      viewCart,
     };
   },
 };
