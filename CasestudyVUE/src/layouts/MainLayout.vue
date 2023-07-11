0d8b7dc42ceeba4de4a5c330b1e816fda18631d4
Gui Miranda <gccardosomiranda@outlook.com>
2023-07-11 11:07:15 -0400

    Adds hamburger navbar menu to main layout

--- CasestudyVUE/src/layouts/MainLayout.vue
+++ CasestudyVUE/src/layouts/MainLayout.vue
@@ -3,10 +3,28 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>INFO3067 Casestudy Application</q-toolbar-title>
+        <q-btn flat round dense icon="reorder" class="q-mr-xs">
+          <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup to="/">
+                <q-item-section>Home</q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup to="brands">
+                <q-item-section>Brands</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
+      <!-- Remove this eventually -->
+      <div class="text-center">
+        <q-avatar class="q-mb-md" style="width: 200px; height: 100px" square>
+          <q-img :src="`/img/logo.png`" />
+        </q-avatar>
+      </div>
       <router-view />
     </q-page-container>
 
