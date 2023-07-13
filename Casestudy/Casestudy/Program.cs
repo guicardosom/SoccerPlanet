b2ad0a489f1dbfbf43861e1d3be54a54f9b862fb
Gui Miranda <gccardosomiranda@outlook.com>
2023-07-13 19:06:54 -0400

    Includes JWT to create access tokens

--- Casestudy/Casestudy/Program.cs
+++ Casestudy/Casestudy/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Casestudy.DAL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,29 @@ builder.Services.AddCors(options =>
     });
 });
 
+// jwt addition
+// get key from settings
+var appSettings = builder.Configuration.GetSection("AppSettings").GetValue<string>("Secret");
+var key = Encoding.ASCII.GetBytes(appSettings);
+// add scheme and options
+builder.Services.AddAuthentication(scheme =>
+{
+    scheme.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    scheme.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(option =>
+{
+    option.RequireHttpsMetadata = false;
+    option.SaveToken = true;
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,11 +61,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseCors(MyAllowSpecificOrigins);
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
