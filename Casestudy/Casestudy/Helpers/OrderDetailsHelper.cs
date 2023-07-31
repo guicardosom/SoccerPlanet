e11441d3840b161099829da44a5b85d6c4dfe735
Gui Miranda <gccardosomiranda@outlook.com>
2023-07-31 11:10:05 -0400

    Adds OrderDetailsHelper class to interact with client-side

---
+++ Casestudy/Casestudy/Helpers/OrderDetailsHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Casestudy.Helpers
+{
+    public class OrderDetailsHelper
+    {
+        public int OrderId { get; set; }
+        public string? ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal Cost { get; set; }
+        public int QtySold { get; set; }
+        public int QtyOrdered { get; set; }
+        public int QtyBackOrdered { get; set; }
+        public string? OrderDate { get; set; }
+    }
+}
